1d00a7bf3b3cd051a58a8146f8104354fda530cd
dread <dread@DESKTOP-BS1AKPM>
2020-09-28 15:58:28 -0700

    Add project files.

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿/// Homework 4
+/// File Name: BMI calculator
+/// @author: Dakota Durst
+/// Date: september 28, 2020
+///
+/// Problem Statement: 
+/// Ask the user to enter their height and weight into a box, use values to calculate BMI and output if user's BMI is in a good range
+///
+///
+/// Overall Plan:
+/// 1) create visual display where user inputs values
+/// 2) assign variables for height and weight
+/// 3) Calulate BMI based on Height and weight
+/// 4) Using If statments, created 3 potential cases based on BMI of user
+/// 5) display popup with BMI value with a color based on its range.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double weight;
+            double height;
+
+            weight = double.Parse(Weightbox.Text);
+            height = double.Parse(Heightbox.Text);
+
+            double BMI = weight * 720 / (height * height);
+
+            if (BMI < 15)
+            {
+                Range.Background = Brushes.Blue;
+                MessageBox.Show("Your BMI is equal to: " + BMI.ToString("0.00") +  " You are below the BMI healthy range");
+
+            }
+            else if (BMI >= 15 &&  BMI <= 26)
+            {
+                Range.Background = Brushes.Green;
+                MessageBox.Show("Your BMI is equal to: " + BMI.ToString("0.00") + " You are inside the BMI healthy range");
+            }
+            else
+            {
+                Range.Background = Brushes.Yellow;
+                MessageBox.Show("Your BMI is equal to: " + BMI.ToString("0.00") + " You are above the BMI healthy range");
+            }
+                
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,61 @@
+﻿/// Homework 4
+/// File Name: Coin toss stats
+/// @author: Dakota Durst
+/// Date: september 28, 2020
+///
+/// Problem Statement: 
+/// Ask the user to enter heads or tails 8 times. program will print out the total number of heads and tails and the percentage each one showed up
+///
+///
+/// Overall Plan:
+/// 1) get user to input h or t 8 times
+/// 2) as they input value increase counter by 1 (if they enter an invalid entry decrease i by 1 to compensate
+/// 3) when counter reaches 8 stop getting inputs
+/// 4 )output number of heads and tails 
+/// 5) calculate and output percent of each one
+
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int HeadCount = 0;
+            int TailCount = 0;
+            string toss;
+            int TOTAL_TOSS = 8;
+
+            for (int i = 0; i < TOTAL_TOSS; i++)
+            {
+                Console.WriteLine("Toss number: " + i + " enter h for heads and t for tails.");
+                toss = Console.ReadLine();
+
+                if (toss == "h")
+                {
+                    HeadCount++;
+
+                }
+                else if (toss == "t")
+                {
+                    TailCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Entry");
+                    i = i - 1;
+                }
+            }
+
+            double percentHeads = (((double)HeadCount / TOTAL_TOSS) * 100);
+            double percentTails = (((double)TailCount / TOTAL_TOSS) * 100);
+
+            Console.WriteLine("Number of heads: " + HeadCount);
+            Console.WriteLine("Number of tails: " + TailCount);
+            Console.WriteLine("Perecentage of Heads " + percentHeads + "%");
+            Console.WriteLine("Percentage of Tails " + percentTails + "%");
+
+        }
+    }
+}
